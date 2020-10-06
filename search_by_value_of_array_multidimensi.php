db96739876a9f3b3d8ab0abafcc0b75c23734325
aldipraddana <aldipraddana@gmail.com>
2020-10-06 20:22:21 +0700

    array manipulation

---
+++ search_by_value_of_array_multidimensi.php
@@ -0,0 +1,40 @@
+<?php
+$data = [
+  [
+    'nama' => 'ucup',
+    'umur' => 18,
+    'no_ind' => 12310
+  ],
+  [
+    'nama' => 'memet',
+    'umur' => 19,
+    'no_ind' => 12312
+  ],
+  [
+    'nama' => 'aldo',
+    'umur' => 20,
+    'no_ind' => 34233
+  ],
+  [
+    'nama' => 'memet',
+    'umur' => 19,
+    'no_ind' => 12321
+  ],
+  [
+    'nama' => 'aldo',
+    'umur' => 12,
+    'no_ind' => 19282
+  ],
+  [
+    'nama' => 'zila',
+    'umur' => 20,
+    'no_ind' => 33233
+  ],
+];
+$keys = array_keys(array_column($data, 'nama'), 'memet');
+
+foreach ($keys as $key => $val) {
+ $result[] = $data[$val];
+}
+
+echo "<pre>";print_r($result);
---
+++ sum_the_contents_of_the_array.js
@@ -0,0 +1,5 @@
+// how to sum the contents of an array in javascript
+// you can use reduce function dude!
+let arraynya = [1, 2, 3, 4, 5];
+let hasil = arraynya.reduce((a, b) => a + b);
+console.log(hasil)
